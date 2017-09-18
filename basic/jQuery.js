bf66b8a272c8a7328fac5fc4544111e1b20bb58b
cangxue <752787086@qq.com>
2017-09-18 17:26:03 +0800

    jQuery简单了解

---
+++ basic/jQuery.js
@@ -0,0 +1,242 @@
+/**
+ * Created by xiaoma on 2017/9/15.
+ */
+
+window.jQuery; // jQuery(selector, context)
+window.$; // jQuery(selector, context)
+$ === jQuery; // true
+typeof ($); // 'function'
+
+
+$; // jQuery(selector, context)
+jQuery.noConflict();
+$; // undefined
+jQuery; // jQuery(selector, context)
+
+
+// 选择器
+// 按ID查找
+// 查找<div id="abc">:
+var div = $('#abc'); // jQuery对象
+var divDom = div.get(0); // 假设存在div，获取第1个DOM元素
+var another = &(divDom); // 重新把DOM包装为jQuery对象
+
+// 按tag查找
+var ps = $('p'); // 返回所有<p>节点
+ps.length; // 数一数页面有多少个<p>节点
+
+// 按class查找
+var a = $('.red'); // 所有节点包含`class="red"`都将返回
+var b = $('.red.green'); //查找同时包含red和green的节点：
+
+// 按属性查找
+var email = $('[name=email]');  // 找出<??? name="email">
+var passwordInput = $('[type=password]'); // 找出<??? type="password">
+var a = $('[items="A B"]'); // 找出<??? items="A B">
+var icons = $('[name^=icon]'); // 找出所有name属性值以icon开头的DOM
+// 例如: name="icon-1", name="icon-2"
+var names = $('[name$=with]'); // 找出所有name属性值以with结尾的DOM
+// 例如: name="startswith", name="endswith"
+var icons = $('[class^="icon-"]'); // 找出所有class包含至少一个以`icon-`开头的DOM
+// 例如: class="icon-clock", class="abc icon-home"
+
+
+// 组合查找
+var emailInput = $('input[name=email]'); // 不会找出<div name="email">
+var tr = $('tr.red'); // 找出<tr class="red ...">...</tr>
+
+// 多项选择器
+$('p,div'); // 把<p>和<div>都选出来
+$('p.red,p.green'); // 把<p class="red">和<p class="green">都选出来
+
+
+// 层级选择器（Descendant Selector）
+<!-- HTML结构 -->
+<div class="testing">
+    <ul class="lang">
+        <li class="lang-javascript">JavaScript</li>
+        <li class="lang-python">Python</li>
+        <li class="lang-lua">Lua</li>
+    </ul>
+</div>
+
+$('ul.lang li.lang-javascript'); // [<li class="lang-javascript">JavaScript</li>]
+$('div.testing li.lang-javascript'); // [<li class="lang-javascript">JavaScript</li>]
+
+$('form.test p input'); // 在form表单选择被<p>包含的<input>
+
+// 子选择器（Child Selector）
+$('ul.lang>li.lang-javascript'); // 可以选出[<li class="lang-javascript">JavaScript</li>]
+$('div.testing>li.lang-javascript'); // [], 无法选出，因为<div>和<li>不构成父子关系
+
+// 过滤器（Filter
+$('ul.lang li'); // 选出JavaScript、Python和Lua 3个节点
+
+$('ul.lang li:first-child'); // 仅选出JavaScript
+$('ul.lang li:last-child'); // 仅选出Lua
+$('ul.lang li:nth-child(2)'); // 选出第N个元素，N从1开始
+$('ul.lang li:nth-child(even)'); // 选出序号为偶数的元素
+$('ul.lang li:nth-child(odd)'); // 选出序号为奇数的元素
+
+$('div:visible'); // 所有可见的div
+$('div:hidden'); // 所有隐藏的div
+
+// 查找和过滤
+<!-- HTML结构 -->
+<ul class="lang">
+    <li class="js dy">JavaScript</li>
+    <li class="dy">Python</li>
+    <li id="swift">Swift</li>
+    <li class="dy">Scheme</li>
+    <li name="haskell">Haskell</li>
+</ul>
+
+// 用find()查找：
+var ul = $('ul.lang'); // 获得<ul>
+var dy = ul.find('.dy') // 获得JavaScript, Python, Scheme
+var swf = ul.find('#swift'); // 获得Swift
+var hsk = ul.find('[name=haskell]'); // 获得Haskell
+
+// 如果要从当前节点开始向上查找，使用parent()方法：
+var swf = $('#swift'); // 获得Swift
+var parent = swf.parent(); // 获得Swift的上层节点<ul>
+var a = swf.parent('div.red'); // 从Swift的父节点开始向上查找，直到找到某个符合条件的节点并返回
+
+// 对于位于同一层级的节点，可以通过next()和prev()方法
+var swift = $('#swift');
+
+swift.next(); // Scheme
+swift.next('[name=haskell]'); // Haskell，因为Haskell是后续第一个符合选择器条件的节点
+
+swift.prev(); // Python
+swift.prev('.js'); // JavaScript，因为JavaScript是往前第一个符合选择器条件的节点
+
+// 过滤
+// filter()方法可以过滤掉不符合选择器条件的节点：
+var langs = $('ul.lang li'); // 拿到JavaScript, Python, Swift, Scheme和Haskell
+var a = langs.filter('.dy'); // 拿到JavaScript, Python, Scheme
+
+var langs = $('ul.lang li'); // 拿到JavaScript, Python, Swift, Scheme和Haskell
+langs.filter(function () {
+    return this.innerHTML.indexOf('S') === 0; // 返回S开头的节点
+}); // 拿到Swift, Scheme
+
+// map()方法把一个jQuery对象包含的若干DOM节点转化为其他对象：
+var langs = $('ul.lang li'); // 拿到JavaScript, Python, Swift, Scheme和Haskell
+var arr = langs.map(function () {
+    return this.innerHTML;
+}).get(); // 用get()拿到包含string的Array：['JavaScript', 'Python', 'Swift', 'Scheme', 'Haskell']
+
+
+var langs = $('ul.lang li'); // 拿到JavaScript, Python, Swift, Scheme和Haskell
+var js = langs.first(); // JavaScript，相当于$('ul.lang li:first-child')
+var haskell = langs.last(); // Haskell, 相当于$('ul.lang li:last-child')
+var sub = langs.slice(2, 4); // Swift, Scheme, 参数和数组的slice()方法一致
+
+
+
+// 操作DOM
+// 修改Text和HTML
+<!-- HTML结构 -->
+<ul id="test-ul">
+    <li class="js">JavaScript</li>
+    <li name="book">Java &amp; JavaScript</li>
+</ul>
+$('#test-ul li[name=book]').text(); // 'Java & JavaScript'
+$('#test-ul li[name=book]').html(); // 'Java &amp; JavaScript'
+
+$('#test-ul li').text('JS');
+
+
+// 修改CSS
+<!-- HTML结构 -->
+<ul id="test-css">
+    <li class="lang dy"><span>JavaScript</span></li>
+    <li class="lang"><span>Java</span></li>
+    <li class="lang dy"><span>Python</span></li>
+    <li class="lang"><span>Swift</span></li>
+    <li class="lang dy"><span>Scheme</span></li>
+</ul>
+
+$('#test-css li.dy>span').css('background-color', '#ffd351').css('color', 'red');
+
+var div = $('#test-div');
+div.css('color'); // '#000033', 获取CSS属性
+div.css('color', '#336699'); // 设置CSS属性
+div.css('color', ''); // 清除CSS属性
+
+var div = $('#test-div');
+div.hasClass('highlight'); // false， class是否包含highlight
+div.addClass('highlight'); // 添加highlight这个class
+div.removeClass('highlight'); // 删除highlight这个class
+
+
+// 显示和隐藏DOM
+var a = $('a[target=_blank]');
+a.hide(); // 隐藏
+a.show(); // 显示
+
+
+// 创建DOM对象:
+var ps = document.createElement('li');
+ps.innerHTML = '<span>Pascal</span>';
+// 添加DOM对象:
+ul.append(ps);
+
+// 添加jQuery对象:
+ul.append($('#scheme'));
+
+// 添加函数对象:
+ul.append(function (index, html) {
+    return '<li><span>Language - ' + index + '</span></li>';
+});
+
+
+// 事件
+/* HTML:
+ *
+ * <a id="test-link" href="#0">点我试试</a>
+ *
+ */
+
+// 获取超链接的jQuery对象:
+var a = $('#test-link');
+a.on('click', function () {
+    alert('Hello!');
+});
+a.click(function () {
+    alert('Hello!');
+});
+
+function hello() {
+    alert('hello!');
+}
+
+a.click(hello); // 绑定事件
+
+// 10秒后解除绑定
+setTimeout(function () {
+    a.off('click', hello);
+}, 10000);
+
+
+var button1 = $('#testPopupButton1');
+var button2 = $('#testPopupButton2');
+
+function popupTestWindow() {
+    window.open('/');
+}
+
+button1.click(function () {
+    popupTestWindow();
+});
+
+button2.click(function () {
+    // 不立刻执行popupTestWindow()，100毫秒后执行:
+    setTimeout(popupTestWindow, 100);
+});
+
+
+
+
+
