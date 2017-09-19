47d13c0874f9822f7bbb18645122eb56621e59c8
cangxue <752787086@qq.com>
2017-09-19 17:31:41 +0800

    collection了解

---
+++ basic/underscore.js
@@ -0,0 +1,100 @@
+/**
+ * Created by xiaoma on 2017/9/19.
+ */
+
+
+// Collections
+
+var obj = {
+    name: 'bob',
+    school: 'No.1 middle school',
+    address: 'xueyuan road'
+};
+
+var upper = _.map(obj, function (value, key) {
+    return key;
+});
+alert(JSON.stringify(upper)); // ["name","school","address"]
+
+// 所有元素大于0
+_.every([1, 4, 7, -3, -9], (x) => x > 0); // false
+// 至少一个元素大于0？
+_.some([1, 4, 7, -3, -9], (x) => x > 0); // true
+
+var arr = [3, 5, 7, 9];
+_.max(arr); // 9
+_.min(arr); // 3
+
+var scores = [20, 81, 75, 40, 91, 59, 77, 66, 72, 88, 99];
+var groups = _.groupBy(scores, function (x) {
+    if (x < 60) {
+        return 'C';
+    } else if (x < 80) {
+        return 'B';
+    } else {
+        return 'A';
+    }
+});
+// 结果:
+// {
+//   A: [81, 91, 88, 99],
+//   B: [75, 77, 66, 72],
+//   C: [20, 40, 59]
+// }
+
+// shuffle()用洗牌算法随机打乱一个集合
+// 注意每次结果都不一样：
+_.shuffle([1, 2, 3, 4, 5, 6]); // [3, 5, 4, 6, 2, 1]
+
+// sample()则是随机选择一个或多个元素：
+// 注意每次结果都不一样：
+// 随机选1个：
+_.sample([1, 2, 3, 4, 5, 6]); // 2
+// 随机选3个：
+_.sample([1, 2, 3, 4, 5, 6], 3); // [6, 1, 4]
+
+var arr = [2, 4, 6, 8];
+_.first(arr); // 2
+_.last(arr); // 8
+
+_.flatten([1, [2], [3, [[4], [5]]]]); // [1, 2, 3, 4, 5]
+
+var names = ['Adam', 'Lisa', 'Bart'];
+var scores = [85, 92, 59];
+_.zip(names, scores);
+// [['Adam', 85], ['Lisa', 92], ['Bart', 59]]
+
+var namesAndScores = [['Adam', 85], ['Lisa', 92], ['Bart', 59]];
+_.unzip(namesAndScores);
+// [['Adam', 'Lisa', 'Bart'], [85, 92, 59]]
+
+
+var names = ['Adam', 'Lisa', 'Bart'];
+var scores = [85, 92, 59];
+_.object(names, scores);
+// {Adam: 85, Lisa: 92, Bart: 59}
+
+
+// 从0开始小于10:
+_.range(10); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// 从1开始小于11：
+_.range(1, 11); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// 从0开始小于30，步长5:
+_.range(0, 30, 5); // [0, 5, 10, 15, 20, 25]
+
+// 从0开始大于-10，步长-1:
+_.range(0, -10, -1); // [0, -1, -2, -3, -4, -5, -6, -7, -8, -9]
+
+
+
+
+
+
+
+
+
+
+
+
