7388276586c2fbe45da8512dbca5ae2a8659553d
cangxue <752787086@qq.com>
2017-09-21 13:40:22 +0800

    hello.js在线测试

---
+++ Node/hello.js
@@ -0,0 +1,7 @@
+/**
+ * Created by xiaoma on 2017/9/20.
+ */
+
+'use strict';
+
+console.log('Hello, world!');
---
+++ demo/js/hello.js
@@ -0,0 +1,10 @@
+/**
+ * Created by xiaoma on 2017/9/21.
+ */
+
+function helloFunction() {
+    document.getElementById("demo").innerHTML="我的第一个 JavaScript 函数 !";
+}
+
+
+
