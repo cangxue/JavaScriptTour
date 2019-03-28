7fea0970572ecd9aed0bae46909f96ca1e9126d9
xiaoma <752787086@qq.com>
2019-03-28 15:22:31 +0800

    change project enum

--- demo/res/js/flex-test.js
+++
@@ -1,4 +0,0 @@
-
-document.getElementsByClassName("w90")[0].addEventListener('click', function() {
-    document.getElementsByClassName("flex-item flex-item1")[0].style.width = "180px"
-}, false)
--- example/myFunction.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by xiaoma on 2017/10/11.
- */
-
-function firstFunction() {
-    document.getElementById("p3").innerHTML = "我的第一个 JavaScript 函数 !";
-}
